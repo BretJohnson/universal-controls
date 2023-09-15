9287326f0246cdd0f6724ba272874e48d93c5e03
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-09-15 13:06:40 +0200

    Created Slider control

---
+++ samples/controls/SimpleControls/Slider/Slider_Examples.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.ComponentModelEx;
+using AnywhereControls;
+using AnywhereControls.Controls;
+using static SimpleControls.SimpleControlsStatics;
+
+namespace AlohaKit.StandardControls
+{
+    public class Slider_Examples
+    {
+        [UIExample("Slider")]
+        public static ISlider SimpleSlider() =>
+            Slider()
+                .Value(5);
+
+        [UIExample("Custom Slider")]
+        public static ISlider CustomSlider() =>
+            Slider()
+                .MinimumColor(Colors.Orange)
+                .MaximumColor(Colors.Blue)
+                .ThumbColor(Colors.Red)
+                .Value(5);
+    }
+}
