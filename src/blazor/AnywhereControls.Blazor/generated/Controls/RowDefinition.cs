b31c3e70412504afc6bd287cd5630c06326ae000
Bret Johnson <bret.johnson@microsoft.com>
2024-01-15 15:48:44 -0500

    Restore blazor generated, from main

---
+++ src/blazor/AnywhereControls.Blazor/generated/Controls/RowDefinition.cs
@@ -0,0 +1,39 @@
+// This file is generated from IRowDefinition.cs. Update the source file to change its contents.
+
+using AnywhereControls.DefaultImplementations;
+using Microsoft.AspNetCore.Components;
+using AnywhereControls.Controls;
+
+namespace AnywhereControls.Blazor.Controls
+{
+    public class RowDefinition : UIObject, IRowDefinition
+    {
+        public static readonly UIProperty HeightProperty = new UIProperty(nameof(Height), GridLength.Default);
+        public static readonly UIProperty MinHeightProperty = new UIProperty(nameof(MinHeight), 0.0);
+        public static readonly UIProperty MaxHeightProperty = new UIProperty(nameof(MaxHeight), double.PositiveInfinity);
+        public static readonly UIProperty ActualHeightProperty = new UIProperty(nameof(ActualHeight), 0.0, readOnly:true);
+        
+        [Parameter]
+        public GridLength Height
+        {
+            get => (GridLength) GetNonNullValue(HeightProperty);
+            set => SetValue(HeightProperty, value);
+        }
+        
+        [Parameter]
+        public double MinHeight
+        {
+            get => (double) GetNonNullValue(MinHeightProperty);
+            set => SetValue(MinHeightProperty, value);
+        }
+        
+        [Parameter]
+        public double MaxHeight
+        {
+            get => (double) GetNonNullValue(MaxHeightProperty);
+            set => SetValue(MaxHeightProperty, value);
+        }
+        
+        public double ActualHeight => (double) GetNonNullValue(ActualHeightProperty);
+    }
+}
