28806b23711abbb30fc72d9c850e367101fcb0e9
Bret Johnson <bret.johnson@microsoft.com>
2024-01-15 15:45:49 -0500

    Remove winforms generated

--- src/winforms/AnywhereControls.WinForms/generated/VisualStateManager.cs
+++
@@ -1,8 +0,0 @@
-// This file is generated from IVisualStateManager.cs. Update the source file to change its contents.
-
-namespace AnywhereControls.WinForms
-{
-    public class VisualStateManager : IVisualStateManager
-    {
-    }
-}
