de40d6f69adad1c23e7a382f0f9937280cda2461
Bret Johnson <bret.johnson@microsoft.com>
2024-01-15 15:41:39 -0500

    Remove other copies of generated files

--- src/AnywhereControls.Maui/generated/VisualStateManager.cs
+++
@@ -1,8 +0,0 @@
-// This file is generated from IVisualStateManager.cs. Update the source file to change its contents.
-
-namespace AnywhereControls.Maui
-{
-    public class VisualStateManager : IVisualStateManager
-    {
-    }
-}
