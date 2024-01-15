2615e4d5a34e7d98e94a40cee73f293c235cfaa9
Bret Johnson <bret.johnson@microsoft.com>
2024-01-15 15:31:32 -0500

    Remove generated code for non-active frameworks

--- src/mac/AnywhereControls.Mac/generated/VisualStateManager.cs
+++
@@ -1,8 +0,0 @@
-// This file is generated from IVisualStateManager.cs. Update the source file to change its contents.
-
-namespace AnywhereControls.Mac
-{
-    public class VisualStateManager : IVisualStateManager
-    {
-    }
-}
